3042cbae95a7f1f52d23eb31cc87e5fab7df146f
4mthfgr78c-svg <4mthfgr78c@privaterelay.appleid.com>
2026-06-10 03:40:21 +1100

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import asyncio
 from aiogram import Bot, Dispatcher
 from config import BOT_TOKEN
-from handlers import router
+from handlers import router, set_bot
 from database import init_db
 
 async def main():
     await init_db()
     
     bot = Bot(token=BOT_TOKEN)
+    set_bot(bot)
+    
     dp = Dispatcher()
     dp.include_router(router)
     
